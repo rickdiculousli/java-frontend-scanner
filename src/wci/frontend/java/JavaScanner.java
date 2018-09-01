dfc4f577b6fe5925ddc753d047267d152580a2c7
Rick Li <li.rick.1996@gmail.com>
2018-09-01 15:18:39 -0700

    implement skipWhiteSpace(JavaScanner)

--- src/wci/frontend/java/JavaScanner.java
+++ src/wci/frontend/java/JavaScanner.java
@@ -4,6 +4,7 @@ import wci.frontend.*;
 import wci.frontend.pascal.tokens.*;
 
 import static wci.frontend.Source.EOF;
+import static wci.frontend.Source.EOL;
 import static wci.frontend.java.JavaTokenType.*;
 import static wci.frontend.java.JavaErrorCode.*;
 
@@ -12,13 +13,58 @@ public class JavaScanner extends Scanner{
 
 	public JavaScanner(Source source) {
 		super(source);
-		// TODO Auto-generated constructor stub
+
 	}
 
 	@Override
 	protected Token extractToken() throws Exception {
-		// TODO Auto-generated method stub
+		
+		skipWhiteSpace();
 		return null;
+	}
+	
+	private void skipWhiteSpace() throws Exception{
+		char currentChar = currentChar();
+		
+		while (Character.isWhitespace(currentChar) || (currentChar == '/')){
+			
+			// Start of a comment?
+			if(currentChar == '/') {
+				currentChar = nextChar();
+				
+				//Is single line comment?
+				if(currentChar == '/') {
+					do {
+						currentChar = nextChar();
+					} while ((currentChar != EOL) && (currentChar != EOF));
+				}
+				
+				//Is multiline comment?
+				else if(currentChar == '*') {
+					boolean end = false;
+					do {
+						currentChar = nextChar();
+						if((currentChar == '*') && (peekChar() == '/')) { //reached the end?
+							end = true;
+							break;
+						}
+					} while (currentChar != EOF);
+					if(end) {
+						currentChar = nextChar(); // consumes '*'
+						currentChar = nextChar(); // consumes '/'
+					}
+				}
+			}
+			
+			// Not a comment.
+			else {
+				currentChar = nextChar(); //consume whitespace character.
+			}
+		}
+	}
+	
+	public char peekChar() throws Exception{
+		return super.source.peekChar();
 	}
 
 }
