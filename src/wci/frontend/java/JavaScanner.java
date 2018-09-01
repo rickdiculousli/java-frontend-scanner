2f571356b707a68358a82df80bbe12c6bee230a9
Rick Li <li.rick.1996@gmail.com>
2018-09-01 15:33:02 -0700

    Partial implement extractToken/Waiting on dependecies

--- src/wci/frontend/java/JavaScanner.java
+++ src/wci/frontend/java/JavaScanner.java
@@ -1,7 +1,8 @@
 package wci.frontend.java;
 
 import wci.frontend.*;
-import wci.frontend.pascal.tokens.*;
+import wci.frontend.java.JavaTokenType;
+import wci.frontend.java.tokens.*;
 
 import static wci.frontend.Source.EOF;
 import static wci.frontend.Source.EOL;
@@ -20,9 +21,43 @@ public class JavaScanner extends Scanner{
 	protected Token extractToken() throws Exception {
 		
 		skipWhiteSpace();
-		return null;
+		
+		Token token;
+		char currentChar = currentChar();
+		 // Construct the next token.  The current character determines the
+        // token type.
+        if (currentChar == EOF) {
+            token = new EofToken(source);
+        }
+        else if (Character.isLetter(currentChar)) {
+            token = new JavaWordToken(source);
+        }
+        else if (Character.isDigit(currentChar)) {
+            token = new JavaNumberToken(source);
+        }
+        else if (currentChar == '\'') {
+            token = new JavaStringToken(source);
+        }
+        // TODO: Wait for TokenType and ErrorCode to be implemented
+        /*****************
+        else if (JavaTokenType.SPECIAL_SYMBOLS
+                 .containsKey(Character.toString(currentChar))) {
+            token = new JavaSpecialSymbolToken(source);
+        }
+        else {
+            token = new JavaErrorToken(source, INVALID_CHARACTER,
+                                         Character.toString(currentChar));
+            nextChar();  // consume character
+        }
+        *******************/
+
+        return token;
 	}
 	
+    /**
+     * Skip whitespace characters by consuming them.  A comment is whitespace.
+     * @throws Exception if an error occurred.
+     */
 	private void skipWhiteSpace() throws Exception{
 		char currentChar = currentChar();
 		
