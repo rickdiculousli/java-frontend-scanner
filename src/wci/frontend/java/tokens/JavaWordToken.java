75fa0733606d5232b705955803fa5a29e8aaae9b
chastin3 <chastin.gammage@gmail.com>
2018-09-09 16:40:05 -0700

    made changes to Java word Token

--- src/wci/frontend/java/tokens/JavaWordToken.java
+++ src/wci/frontend/java/tokens/JavaWordToken.java
@@ -1,15 +1,71 @@
 package wci.frontend.java.tokens;
 
 import wci.frontend.*;
+import wci.frontend.java.JavaToken;
 import wci.frontend.java.*;
 
 import static wci.frontend.java.JavaTokenType.*;
 
 public class JavaWordToken extends JavaToken{
 
-	public JavaWordToken(Source source) throws Exception {
+	 /**
+     * Constructor.
+     * @param source the source from where to fetch the token's characters.
+     * @throws Exception if an error occurred.
+     */
+	
+	public JavaWordToken(Source source) 
+			throws Exception 
+	{
+	  
 		super(source);
-		// TODO Auto-generated constructor stub
+		
 	}
+	    
+	    /**
+	     * Extract a Java word token from the source.
+	     * @throws Exception if an error occurred.
+	     */
+
+	    protected void extract()
+
+	            throws Exception
+
+	    {
+
+	        StringBuilder textBuffer = new StringBuilder();
+
+	        char currentChar = currentChar();
+
+
+
+	        // Get the word characters (letter or digit).  The scanner has
+
+	        // already determined that the first character is a letter.
+
+	        while (Character.isLetterOrDigit(currentChar)) {
+
+	            textBuffer.append(currentChar);
+
+	            currentChar = nextChar();  // consume character
+
+	        }
+
+
+
+	        text = textBuffer.toString();
+
+
+
+	        // Is it a reserved word or an identifier?
+
+	        type = (RESERVED_WORDS.contains(text))
+
+	                ? JavaTokenType.valueOf(text.toUpperCase())  // reserved word
+
+	                : IDENTIFIER;                                  // identifier
+
+	    }
 
 }
+	
