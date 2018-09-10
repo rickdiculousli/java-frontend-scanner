a3bfb924809ea75cf5d56afff2aa26b8354a5cf5
Axel Hugo <axelhugo96@gmail.com>
2018-09-09 17:24:18 -0700

    Java Parser

--- src/wci/frontend/java/JavaParserTD.java
+++ src/wci/frontend/java/JavaParserTD.java
@@ -8,22 +8,68 @@ import static wci.frontend.java.JavaErrorCode.*;
 import static wci.message.MessageType.*;
 
 public class JavaParserTD extends Parser{
+	
+    protected static JavaErrorHandler errorHandler = new JavaErrorHandler();
 
-	protected JavaParserTD(Scanner scanner) {
-		super(scanner);
-		// TODO Auto-generated constructor stub
-	}
+	 /**
+     * Constructor.
+     * @param scanner the scanner to be used with this parser.
+     */
+    public JavaParserTD(Scanner scanner)
+    {
+        super(scanner);
+    }
+	
+	/**
+     * Parse a Java source program and generate the symbol table
+     * and the intermediate code.
+     */
+    public void parse()
+            throws Exception
+        {
+            Token token;
+            long startTime = System.currentTimeMillis();
 
-	@Override
-	public void parse() throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+            try {
+                // Loop over each token until the end of file.
+                while (!((token = nextToken()) instanceof EofToken)) {
+                    TokenType tokenType = token.getType();
 
-	@Override
+                    if (tokenType != ERROR) {
+
+                        // Format each token.
+                        sendMessage(new Message(TOKEN,
+                                                new Object[] {token.getLineNumber(),
+                                                              token.getPosition(),
+                                                              tokenType,
+                                                              token.getText(),
+                                                              token.getValue()}));
+                    }
+                    else {
+                        errorHandler.flag(token, (JavaErrorCode) token.getValue(),
+                                          this);
+                    }
+
+                }
+
+                // Send the parser summary message.
+                float elapsedTime = (System.currentTimeMillis() - startTime)/1000f;
+                sendMessage(new Message(PARSER_SUMMARY,
+                                        new Number[] {token.getLineNumber(),
+                                                      getErrorCount(),
+                                                      elapsedTime}));
+            }
+            catch (java.io.IOException ex) {
+                errorHandler.abortTranslation(IO_ERROR, this);
+            }
+        }
+
+	/**
+     * Return the number of syntax errors found by the parser.
+     * @return the error count.
+     */
 	public int getErrorCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return errorHandler.getErrorCount();
 	}
 
 }
